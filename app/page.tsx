a0555be4b0127c19b601f2a86ec95105754fb402
jeetsingh008 <iamjeetsingh46@gmail.com>
2025-02-13 23:44:20 +0530

    added productlist section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import ProductsOverview from "./products/page";
 
 export default function Home() {
   return (
     <div>
       <main className="overflow-hidden">
         <Hero />
+        <ProductsOverview />
       </main>
     </div>
   );
