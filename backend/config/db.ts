a652e1d311fc276c561f60a101361b121ad71583
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-08-07 08:40:53 +0530

    Made tsconfig file for typescript, added type of express

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI as string);
+    const conn = mongoose.connect(process.env.MONGO_URI!);
     console.log(`Mongoose connected: ${mongoose.connection.host}`);
   } catch (error) {
     console.log("MongoDB Connection failed ", error);
